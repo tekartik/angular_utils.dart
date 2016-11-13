132e35a8ce380502738dbfdaf10be293ee9e053b
Alexandre Roux Tekartik <alex@tekartik.com>
2016-11-13 09:25:57 +0100

    feat: sidebar build

--- tool/grind.dart
+++ tool/grind.dart
@@ -2,10 +2,20 @@ import 'package:tekartik_build_utils/grind/grind_app.dart';
 export 'package:tekartik_build_utils/grind/grind_app.dart';
 
 main(List<String> args) {
+//  example_sidebar();
+app.gsPath = "gs://gs.tk4k.ovh/angular2_utils";
+example();
+example_sidebar();
   grind(args);
 }
 
+example_sidebar() {
+  app.path = join("example", "side_bar");
+  app.gsPath = "gs://gs.tk4k.ovh/angular2_utils/example/side_bar";
+
+}
 @Task('Test')
 test() async {
   devPrint('test');
 }
+
