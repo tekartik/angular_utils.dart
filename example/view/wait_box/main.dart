bb3a72a265ae1d62ac321757f3ef8cd3f9caddab
Alexandre Roux <alex@tekartik.com>
2018-12-17 09:34:46 +0100

    fix: remove material providers from wait box

--- example/view/wait_box/main.dart
+++ example/view/wait_box/main.dart
@@ -5,7 +5,7 @@ import 'package:angular/angular.dart';
 import 'package:tekartik_app_utils/css_app_reset/loader.dart';
 import 'package:tekartik_app_utils/iron_flex_layout/loader.dart';
 
-import 'package:tekartik_angular_utils/app_component.template.dart' as ng;
+import 'app/app.template.dart' as ng;
 
 main() async {
   await loadCssAppResetCss();
